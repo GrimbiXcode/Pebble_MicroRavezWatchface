8cc23ff616977d6281e41edaa719faefa11158fa
David Grimbichler <david@grimbichler.ch>
2015-11-09 08:19:13 +0100

    Date & Font
    - add date (day and month)
    - set new font LECO

--- src/main.c
+++ src/main.c
@@ -6,14 +6,13 @@
 * First Release:    2015-07-30
 ********************************************************************/
 
-
 /********************************************************************
 * Includes
 *
 ********************************************************************/
 #include <pebble.h>
 #include <pebble_fonts.h>
-  
+
   
 /********************************************************************
 * Variables
@@ -21,6 +20,8 @@
 ********************************************************************/
 static Window *s_main_window;
 static TextLayer *s_time_layer;
+static TextLayer *s_date_d_layer;
+static TextLayer *s_date_m_layer;
 static TextLayer *s_battery_layer;
 static GBitmap *s_background_bitmap;
 static BitmapLayer *s_background_bitmap_layer;
@@ -45,10 +46,16 @@ static void update_time() {
   // Get a tm structure
   time_t temp = time(NULL); 
   struct tm *tick_time = localtime(&temp);
-
+  
   // Create a long-lived buffer
   static char buffer[] = "00:00";
+  static char buffer_d[] = "00";
+  static char buffer_m[] = "00";
 
+  // Write the current date (day and month)
+  strftime(buffer_d, sizeof("00"), "%e", tick_time);
+  strftime(buffer_m, sizeof("00"), "%m", tick_time);
+  
   // Write the current hours and minutes into the buffer
   if(clock_is_24h_style() == true) {
     // Use 24 hour format
@@ -60,6 +67,8 @@ static void update_time() {
 
   // Display this time on the TextLayer
   text_layer_set_text(s_time_layer, buffer);
+  text_layer_set_text(s_date_d_layer, buffer_d);
+  text_layer_set_text(s_date_m_layer, buffer_m);
 }
 
 /*===================================================================
@@ -86,14 +95,20 @@ static void battery_handler(BatteryChargeState charge_state) {
 
   if (charge_state.is_charging) {
     snprintf(battery_text, sizeof(battery_text), "charging");
+    text_layer_set_text(s_battery_layer, battery_text);
   } else {
-    snprintf(battery_text, sizeof(battery_text), "%d%% charged", charge_state.charge_percent);
+    if(charge_state.is_plugged){
+      snprintf(battery_text, sizeof(battery_text), "ready");
+      text_layer_set_text(s_battery_layer, battery_text);
+    }
+    //snprintf(battery_text, sizeof(battery_text), "%d%% charged", charge_state.charge_percent);
+    
+    snprintf(battery_text, sizeof(battery_text), " ");
+    text_layer_set_text(s_battery_layer, battery_text);
   }
-  text_layer_set_text(s_battery_layer, battery_text);
-  
   // Save Battery Level
   s_batteryLevel = (charge_state.charge_percent/10)*13; // max 130
-  
+
   // Call draw-update handler
   layer_set_update_proc(s_batteryBarLayer, layer_update_proc);
   
@@ -132,24 +147,38 @@ static void main_window_load(Window *window){
   #endif 
   
   // Create time TextLayer
-  s_time_layer = text_layer_create(GRect(0, 130, bounds.size.w, 30));
+  s_time_layer = text_layer_create(GRect(0, 130, bounds.size.w, 35));
   text_layer_set_background_color(s_time_layer, GColorClear);
   text_layer_set_text_color(s_time_layer, GColorFromRGB(19,168,73));
   text_layer_set_text(s_time_layer, "00:00");
+  
+  //Create date TextLayer
+  s_date_d_layer = text_layer_create(GRect(0, 30, bounds.size.w, 30));
+  text_layer_set_background_color(s_date_d_layer, GColorClear);
+  text_layer_set_text_color(s_date_d_layer, GColorFromRGB(19,168,73));
+  text_layer_set_text(s_date_d_layer, "00");
+  s_date_m_layer = text_layer_create(GRect(0, 30, bounds.size.w, 30));
+  text_layer_set_background_color(s_date_m_layer, GColorClear);
+  text_layer_set_text_color(s_date_m_layer, GColorFromRGB(19,168,73));
+  text_layer_set_text(s_date_m_layer, "00");
 
   // Improve the layout to be more like a watchface
-  text_layer_set_font(s_time_layer, fonts_get_system_font(FONT_KEY_BITHAM_30_BLACK));
+  text_layer_set_font(s_time_layer, fonts_get_system_font(FONT_KEY_LECO_32_BOLD_NUMBERS));
   text_layer_set_text_alignment(s_time_layer, GTextAlignmentCenter);
+  text_layer_set_font(s_date_d_layer, fonts_get_system_font(FONT_KEY_LECO_20_BOLD_NUMBERS));
+  text_layer_set_text_alignment(s_date_d_layer, GTextAlignmentLeft);
+  text_layer_set_font(s_date_m_layer, fonts_get_system_font(FONT_KEY_LECO_20_BOLD_NUMBERS));
+  text_layer_set_text_alignment(s_date_m_layer, GTextAlignmentRight);
   
   // Create battery TextLayer
-  s_battery_layer = text_layer_create(GRect(0, 0, bounds.size.w, bounds.size.h));
+  s_battery_layer = text_layer_create(GRect(0, 6, bounds.size.w, bounds.size.h-6));
   text_layer_set_background_color(s_battery_layer, GColorClear);
   text_layer_set_text_color(s_battery_layer, GColorFromRGB(19,168,73));
   text_layer_set_text_alignment(s_battery_layer, GTextAlignmentCenter);
   
   // Improve the layout to be more like a battery state
-  text_layer_set_font(s_battery_layer, fonts_get_system_font(FONT_KEY_GOTHIC_18));
-  text_layer_set_text_alignment(s_time_layer, GTextAlignmentCenter);
+  text_layer_set_font(s_battery_layer, fonts_get_system_font(FONT_KEY_GOTHIC_14));
+  text_layer_set_text_alignment(s_battery_layer, GTextAlignmentCenter);
   
   // Create BatteryBarLayer
   s_path = gpath_create(&PATH_INFO);
@@ -159,6 +188,8 @@ static void main_window_load(Window *window){
   // Add Layers as a child layer to the Window's root layer
   layer_add_child(window_layer, bitmap_layer_get_layer(s_background_bitmap_layer));
   layer_add_child(window_layer, text_layer_get_layer(s_time_layer));
+  layer_add_child(window_layer, text_layer_get_layer(s_date_d_layer));
+  layer_add_child(window_layer, text_layer_get_layer(s_date_m_layer));
   layer_add_child(window_layer, text_layer_get_layer(s_battery_layer));
   layer_add_child(window_layer, s_batteryBarLayer);
   
@@ -178,6 +209,8 @@ static void main_window_unload(Window *window){
   gbitmap_destroy(s_background_bitmap);
   // Destroy TextLayer
   text_layer_destroy(s_time_layer);
+  text_layer_destroy(s_date_d_layer);
+  text_layer_destroy(s_date_m_layer);
   text_layer_destroy(s_battery_layer);
   // Destroy BatteryBarlayer
   layer_destroy(s_batteryBarLayer);
